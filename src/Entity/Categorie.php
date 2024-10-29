275f217db1aceaf4d54d46062aa44fb421c3537f
kvngbdb <kevin.nguebdob@gmail.com>
2024-10-29 21:46:09 +0100

    DTO

---
+++ src/Entity/Categorie.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategorieRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+
+#[ORM\Entity(repositoryClass: CategorieRepository::class)]
+class Categorie
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+  
+    #[Assert\NotBlank(message: 'Le nom de la catégorie ne peut pas être vide.')]
+    #[ORM\Column(length:40,unique:true)]
+    private ?string $nomCategorie = null;
+
+    #[ORM\Column]
+    private ?bool $isActive = false;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNomCategorie(): ?string
+    {
+        return $this->nomCategorie;
+    }
+
+    public function setNomCategorie(string $nomCategorie): static
+    {
+        $this->nomCategorie = $nomCategorie;
+
+        return $this;
+    }
+
+    public function isActive(): ?bool
+    {
+        return $this->isActive;
+    }
+
+    public function setActive(bool $isActive): static
+    {
+        $this->isActive = $isActive;
+
+        return $this;
+    }
+}
